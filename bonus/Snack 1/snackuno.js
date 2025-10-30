159f6576300c1de5b2e3fb889731d4018fc32bec
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-10-30 15:50:20 +0100

    completo snack 1

--- bonus/Snack 1/snackuno.js
+++ bonus/Snack 1/snackuno.js
@@ -2,3 +2,21 @@
 Inserisci un numero,
  se è pari stampa il numero, 
  se è dispari stampa il numero successivo.*/
+
+/* faccio inseriere un numero dall'utente */
+/* controlliamo che sia un numero */
+/* se è pari stampo il numero */
+/* se è dispari stampo il numero successivo */
+
+const num1 = parseInt(prompt("Inserisci un numero"));
+if (isNaN(num1)) {
+  alert("Inserisci un numero");
+} else {
+  let messaggio;
+  if (num1 % 2 === 0) {
+    messaggio = num1;
+  } else {
+    messaggio = num1 + 1;
+  }
+  console.log(messaggio);
+}
