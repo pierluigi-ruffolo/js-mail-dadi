faac3a95bf62f95575a62b1a5ace10dcd87a2302
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-10-30 13:16:03 +0100

    first commit

---
+++ Esercizio di oggi/javascript.js
@@ -0,0 +1,11 @@
+/*  Mail
+Crea un array con delle email di chi può acedere.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email.
+NON USARE INCLUDES! */
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.  */
---
+++ bonus/bonus.js
@@ -0,0 +1,46 @@
+/* !!!BONUS!!! (create la cartella separata nella stessa repository per questi esercizi)
+Snack 1
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+Snack 2
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+
+Snack 3
+Crea una variabile con un numero di 4 cifre e calcola la somma di tutte le cifre che compongono il numero.
+
+Snack 4
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
+
+Snack 5
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di 5 invitati con nome e cognome.
+
+const nomi = [
+  "Albert",
+  "Leonardo",
+  "Marilyn",
+  "Michael",
+  "Serena",
+  "Elon",
+  "Frida",
+  "Steve",
+  "Beyoncé",
+  "David"
+];
+
+const cognomi = [
+  "Einstein",
+  "da Vinci",
+  "Monroe",
+  "Jackson",
+  "Williams",
+  "Musk",
+  "Kahlo",
+  "Jobs",
+  "Knowles",
+  "Beckham"
+];
+
+Snack 6
+Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro. */
